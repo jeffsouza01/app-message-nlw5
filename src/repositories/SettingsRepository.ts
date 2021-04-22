ef0f7d298e7766b164a6349f4e8920b5fd668bf8
Jefferson da Silva <je_jss@hotmail.com>
2021-04-22 20:40:09 -0300

    Innitial commit

---
+++ src/repositories/SettingsRepository.ts
@@ -0,0 +1,10 @@
+import { Setting } from "../entities/Setting"
+import { EntityRepository, Repository } from "typeorm";
+
+@EntityRepository(Setting)
+class SettingsRepository extends Repository<Setting> {
+
+
+}
+
+export { SettingsRepository }
