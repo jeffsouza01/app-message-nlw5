37b798911fab4cc19eff6e81b0740126a4895e18
Jefferson da Silva <je_jss@hotmail.com>
2021-04-23 01:41:38 -0300

    Create users route

---
+++ src/services/SettingsService.ts
@@ -0,0 +1,34 @@
+import { getCustomRepository } from "typeorm";
+import { SettingsRepository } from "../repositories/SettingsRepository";
+
+interface ISettingsCreate {
+  chat: boolean;
+  username: string;
+}
+
+class SettingsService {
+
+  async create({chat, username}: ISettingsCreate) {
+
+    const settingsRepository = getCustomRepository(SettingsRepository);
+
+    const userAlreadyExists = await settingsRepository.findOne({username})
+
+    if(userAlreadyExists) {
+      throw new Error ("Username already exists")
+    }
+
+    const settings = settingsRepository.create({
+    username,
+    chat
+    })
+
+    await settingsRepository.save(settings);
+
+    return settings;
+
+  }
+
+}
+
+export { SettingsService}
